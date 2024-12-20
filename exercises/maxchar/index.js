91a811e21d2a521c2d3114f0db7d1fe1b8935647
s-sufi <saeed.sufi@hotmail.com>
2024-12-20 22:59:15 +0330

    do maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,35 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {}
+  const strArr = str.split('')
+
+  for (const key of strArr) {
+    /* if (key in obj) {
+      obj[key]++
+    } else {
+      obj[key] = 1
+    } */
+    // obj[key] = obj[key] + 1 || 1;
+    obj[key] = obj[key] ? obj[key] + 1 : 1
+  }
+  let max; 
+  for (const prop in obj) {
+    max = obj[prop]
+    break
+  }
+
+  let key;
+  for (const prop in obj) {
+    if (obj[prop] >= max) {
+      max = obj[prop]
+      key = prop
+    }
+  }
+  return key; 
+}
+console.log(maxChar('apple 121111111 aaaaaaaaa'))
+console.log(maxChar('abcdefghijklmnaaaaa'))
 
 module.exports = maxChar;
